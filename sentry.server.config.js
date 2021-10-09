91e7247d052032da18db1097c83ec74ea253461b
AleksaC <aleksacukovic1@gmail.com>
2021-10-09 20:37:24 +0200

    Disable sentry on server in dev

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,6 +8,9 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  enabled:
+    process.env.NODE_ENV === "production" &&
+    process.env.SENTRY_ENVIRONMENT !== "development",
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
